6c0d478bdf5c085e0ee8098fa675aff67a8e9de4
Tanmoy <tanmoy3.ts3@gmail.com>
2021-02-22 00:20:04 +0600

    Change Password

--- app/src/main/java/com/example/pstuinfomanageforhall/Profile.java
+++ app/src/main/java/com/example/pstuinfomanageforhall/Profile.java
@@ -1,12 +1,22 @@
 package com.example.pstuinfomanageforhall;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -18,6 +28,8 @@ public class Profile extends AppCompatActivity {
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userID;
+    Button mChangePassButton;
+    FirebaseUser user;
 
 
     @Override
@@ -29,11 +41,13 @@ public class Profile extends AppCompatActivity {
         mStudentId=findViewById(R.id.profileStudentId);
         mEmail=findViewById(R.id.profileEmail);
         mPhone=findViewById(R.id.profilePhone);
+        mChangePassButton=findViewById(R.id.changePassButton);
 
         fAuth=FirebaseAuth.getInstance();
         fStore=FirebaseFirestore.getInstance();
 
         userID=fAuth.getCurrentUser().getUid();
+        user=fAuth.getCurrentUser();
 
         DocumentReference documentReference=fStore.collection("users").document(userID);
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
@@ -44,6 +58,44 @@ public class Profile extends AppCompatActivity {
                 mEmail.setText(documentSnapshot.getString("email"));
                 mPhone.setText(documentSnapshot.getString("phone"));
 
+            }
+        });
+
+        mChangePassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText changePass = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Change Password");
+                passwordResetDialog.setMessage("Enter new password");
+                passwordResetDialog.setView(changePass);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String newPass=changePass.getText().toString();
+                        user.updatePassword(newPass).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Profile.this, "Password changed successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Profile.this, "Password change failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                passwordResetDialog.create().show();
             }
         });
     }
