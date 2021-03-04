3af0e35e6acf833e7a1158de6263b2cb144d03d2
Tanmoy <tanmoy3.ts3@gmail.com>
2021-03-04 22:34:06 +0600

    Update profile picture

--- app/src/main/java/com/example/pstuinfomanageforhall/Profile.java
+++ app/src/main/java/com/example/pstuinfomanageforhall/Profile.java
@@ -5,11 +5,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,14 +27,20 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class Profile extends AppCompatActivity {
     TextView mName, mStudentId, mEmail, mPhone;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userID;
-    Button mChangePassButton;
+    Button mChangePassButton, mChangeProfileButton;
     FirebaseUser user;
+    ImageView mProfileImage;
+    StorageReference storageReference;
 
 
     @Override
@@ -42,9 +53,19 @@ public class Profile extends AppCompatActivity {
         mEmail=findViewById(R.id.profileEmail);
         mPhone=findViewById(R.id.profilePhone);
         mChangePassButton=findViewById(R.id.changePassButton);
+        mProfileImage=findViewById(R.id.profileImage);
+        mChangeProfileButton=findViewById(R.id.changeProfileButton);
 
         fAuth=FirebaseAuth.getInstance();
         fStore=FirebaseFirestore.getInstance();
+        storageReference= FirebaseStorage.getInstance().getReference();
+        StorageReference profileRef=storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(mProfileImage);
+            }
+        });
 
         userID=fAuth.getCurrentUser().getUid();
         user=fAuth.getCurrentUser();
@@ -57,7 +78,6 @@ public class Profile extends AppCompatActivity {
                 mStudentId.setText(documentSnapshot.getString("studentID"));
                 mEmail.setText(documentSnapshot.getString("email"));
                 mPhone.setText(documentSnapshot.getString("phone"));
-
             }
         });
 
@@ -96,6 +116,47 @@ public class Profile extends AppCompatActivity {
                 });
 
                 passwordResetDialog.create().show();
+            }
+        });
+
+        mChangeProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent,1000);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==1000){
+            if(resultCode== Activity.RESULT_OK){
+                Uri imageUri=data.getData();
+                mProfileImage.setImageURI(imageUri);
+                uploadImageToFirebase(imageUri);
+            }
+        }
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        StorageReference fileRef=storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(Profile.this, "Image Uploaded", Toast.LENGTH_SHORT).show();
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(mProfileImage);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Profile.this, "Failed", Toast.LENGTH_SHORT).show();
             }
         });
     }
